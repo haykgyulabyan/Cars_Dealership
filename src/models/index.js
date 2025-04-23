0d7a8772fd4dea933074d0d1836acf313c646276
haykgyulabyan <gyulabyanh@gmail.com>
2025-04-23 15:03:42 +0400

    define relations for new and existing models

--- src/models/index.js
+++ src/models/index.js
@@ -2,13 +2,28 @@ import Car from './car.js';
 import Feature from './feature.js';
 import Model from './model.js';
 import Make from './make.js';
+import User from './user.js';
+import Dealership from './dealership.js';
+import Rating from './rating.js';
+import DealershipUser from './dealership_user.js';
 
 Car.belongsTo(Model, { foreignKey: 'model_id' });
-
 Model.hasMany(Car, { foreignKey: 'model_id' });
 
-Model.hasOne(Make, { foreignKey: 'id', sourceKey: 'make_id' });
+Make.hasMany(Model, { foreignKey: 'make_id' });
+Model.belongsTo(Make, { foreignKey: 'make_id' });
 
 Car.belongsToMany(Feature, { through: 'car_features', foreignKey: 'car_id', timestamps: false });
-
 Feature.belongsToMany(Car, { through: 'car_features', foreignKey: 'feature_id', timestamps: false });
+
+Dealership.hasMany(Car, { foreignKey: 'dealership_id' });
+Car.belongsTo(Dealership, { foreignKey: 'dealership_id' });
+
+Dealership.belongsToMany(User, { through: DealershipUser, foreignKey: 'dealership_id' });
+User.belongsToMany(Dealership, { through: DealershipUser, foreignKey: 'user_id' });
+
+User.hasMany(Rating, { foreignKey: 'user_id' });
+Rating.belongsTo(User, { foreignKey: 'user_id' });
+
+Car.hasMany(Rating, { foreignKey: 'car_id' });
+Rating.belongsTo(Car, { foreignKey: 'car_id' });
